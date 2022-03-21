be0c19149a04c4ad16dfad5e4feb99a740a455ad
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-21 15:35:32 -0500

    categorizing data for training purposes

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ import tensorflow
 import random
 import json
 
+# nltk.download('punkt')
+
 stemmer = LancasterStemmer()
 
 # Load in the json file
 with open('intents.json') as file:
     data = json.load(file)
 
-print(data['intents'][0])
+print(data["intents"][0])
+
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs.append(pattern)
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
