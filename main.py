ac0676a218525479bc51cc368fcc481c13095cc8
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-22 13:07:22 -0500

    get data ready to feed the neural network model

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ stemmer = LancasterStemmer()
 with open('intents.json') as file:
     data = json.load(file)
 
-print(data["intents"][0])
+# print(data["intents"][0])
 
 words = []
 labels = []
@@ -30,8 +30,31 @@ for intent in data["intents"]:
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
-
+# print(words)
+print(docs_x)
 words = [stemmer.stem(w.lower()) for w in words]
-words = sorted(list(set(words)))
+
+# words = sorted(list(set(words)))
+words = list(set(words))
+print(words)
 
 labels = sorted(labels)
+# print(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    # Copy the array
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
