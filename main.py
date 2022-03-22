158bdeecfd193133214cd40a4fc352a72e1ad77f
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-22 13:30:16 -0500

    error fix

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
-# print(words)
+
 print(docs_x)
 words = [stemmer.stem(w.lower()) for w in words]
 
@@ -48,12 +48,14 @@ out_empty = [0 for _ in range(len(labels))]
 
 for x, doc in enumerate(docs_x):
     bag = []
-    wrds = [stemmer.stem(w) for w in doc]
+    wrds = [stemmer.stem(w) for w in doc if w != "?"]
+    # print(wrds)
     for w in words:
         if w in wrds:
             bag.append(1)
         else:
             bag.append(0)
+    # print(bag)
     # Copy the array
     output_row = out_empty[:]
     output_row[labels.index(docs_y[x])] = 1
